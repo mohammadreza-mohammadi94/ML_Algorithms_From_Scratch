756997cd3a27f7917e7c7b4652d65dc33e64467c
jigsaw1313 <mr.mhmdi93@gmail.com>
2024-11-19 17:26:16 +0330

    issue fix

--- Regression/base.py
+++ Regression/base.py
@@ -1,4 +1,11 @@
+#---------------------------------------------------------------------#
+#                     Code By Mohammadreza Mohammadi                  #
+#      Github:   https://github.com/mohammadreza-mohammadi94          #
+#      LinkedIn: https://www.linkedin.com/in/mohammadreza-mhmdi/      #
+#---------------------------------------------------------------------#
+
 import numpy as np
+
 
 class BaseRegression:
     """
@@ -22,7 +29,7 @@ class BaseRegression:
 
         params:
         --------------------------------
-        X: numpy.nbarray
+        X: numpy.ndarray
             Input feature matrix of shape (m,n)
 
         Returns:
@@ -39,9 +46,10 @@ class BaseRegression:
         if self.theta is None:
             raise ValueError("Model has not been fitted yet.")
         
+        # Check if the intercept term is already included
         if X.shape[1] + 1 == self.theta.shape[0]:
-            X = np.c_[np.ones(X.shape[0], 1), X]
+            X = np.c_[np.ones(X.shape[0]), X]
         
-        # Calculate the dot product of the X and theta to produce the
-        # target values
-        return X.dot(self.theta) # np.dot(X, self.theta)
+        # Calculate the dot product of X and theta to produce the target values
+        return X.dot(self.theta)  # or np.dot(X, self.theta)
+
