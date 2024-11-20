eb25082b02d5c1dd572c383df243fa5a27e63928
jigsaw1313 <mr.mhmdi93@gmail.com>
2024-11-20 19:32:12 +0330

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='ML_Algorithms_From_Scratch',
+        version='1.0',
+        packages=find_packages(),
+        author="Mohammadreza Mohammadi",
+        author_email="mr.mhmdi93@gmail.com"
+    )
