ec954272484bd0903d1c243f9783206b15965c72
jigsaw1313 <mr.mhmdi93@gmail.com>
2024-11-23 16:57:22 +0330

    Update BaseRegression with _add_bias method.

--- Regression/base.py
+++ Regression/base.py
@@ -16,14 +16,33 @@ class BaseRegression:
     theta : numpy.ndarray
         Model parameters (weights and bias).
     """
-    def __init__(self):
+    def __init__(self, learning_rate=0.01, n_iterations=1000):
         """
         Initialize the Regression model.
         """
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
         self.theta = None # Model's coefficients
 
+    
+    def _add_bias(self, X):
+        """
+        Adds a bias term (column of ones) to the input feature matrix.
+
+        Parameters:
+        ----------
+        X : numpy.ndarray
+            Input feature matrix.
+
+        Returns:
+        -------
+        numpy.ndarray
+            Feature matrix with bias term.
+        """
+        return np.c_[np.ones((X.shape[0], 1)), X]
+
     # Prediction method
-    def predict(self, X):
+    def predict(self, X, add_bias=True):
         """
         Predict the target values for input features X.
 
@@ -42,14 +61,7 @@ class BaseRegression:
         ValueError:
             if the model is not trained yet then theta is none.
         """
-
-        if self.theta is None:
-            raise ValueError("Model has not been fitted yet.")
-        
-        # Check if the intercept term is already included
-        if X.shape[1] + 1 == self.theta.shape[0]:
-            X = np.c_[np.ones(X.shape[0]), X]
-        
         # Calculate the dot product of X and theta to produce the target values
-        return X.dot(self.theta)  # or np.dot(X, self.theta)
-
+        if add_bias:
+            X = self._add_bias(X)  # Add bias only if needed
+        return X.dot(self.theta)  # Compute predictions# or np.dot(X, self.theta)
